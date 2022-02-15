705901627684cf708aa0fe6e477876121ae57264
borgeer <ariel.borgeer@gmail.com>
2022-02-15 17:52:05 -0300

    ATUALIZADA

---
+++ calculadora.py
@@ -0,0 +1,39 @@
+print ("1= SOMA, 2= DIVISAO, 3= SUBTRAÇÃO, 4= MULTIPLICAÇÃO 5=POTENCIA ")
+op = int(input("Digite o numero da operação que gostaria de fazer: "))
+
+
+while op>5 or op<=0:
+    print("Digite um numero válido de operação ")
+    op = int(input("Digite o numero da operação que gostaria de fazer: "))
+
+
+if op==1:
+    soma=0
+    a=int(input("Digite primeiro numero: "))
+    b=int(input("Digite o segundo numero: "))
+    soma= a+b
+    print (f'A soma de {a}+{b}= {soma}')
+
+if op==2:
+    a=int(input("Digite primeiro numero: "))
+    b=int(input("Digite o segundo numero: "))
+    divisao = a/b
+    print (f'A divisão de {a}/{b}= {divisao:.2f}')
+
+if op==3:
+    a=int(input("Digite primeiro numero: "))
+    b=int(input("Digite o segundo numero: "))
+    sub=a-b
+    print (f' A subtração de {a}-{b}={sub} ')
+
+if op==4:
+    a=int(input("Digite primeiro numero: "))
+    b=int(input("Digite o segundo numero: "))
+    mul=a*b
+    print (f'A multicação de {a}x{b}={mul}')
+
+if op==5:
+    a=int(input("Digite primeiro numero: "))
+    b=int(input("Digite o segundo numero: "))
+    ex=a**b
+    print(f'O numero {a} elevado a {b} é {ex}')
